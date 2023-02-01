7484f524759b107d6d7a547fdd1b1eb5d14a1182
GalipBulan <galipbulan@gmail.com>
2023-02-01 23:58:52 +0100

    01.02.2023

---
+++ src/test/java/test/C01_Get_ApiSorgulama.java
@@ -0,0 +1,44 @@
+package test;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+
+public class C01_Get_ApiSorgulama {
+
+    /*
+        https://restful-booker.herokuapp.com/booking/256884 url’ine
+        bir GET request gonderdigimizde donen Response’un,
+        status code’unun 200,
+        ve content type’inin application/json; charset=utf-8,
+        ve Server isimli Header’in degerinin Cowboy,
+        ve status Line’in HTTP/1.1 200 OK
+        ve response suresinin 5 sn’den kisa oldugunu manuel olarak test ediniz.
+    */
+
+    @Test public void get01(){
+
+        // 1 - Gonderecegimiz Request icin gerekli olan URL ve ihtiyacimiz varsa Body hazirla.
+
+        String url = "https://restful-booker.herokuapp.com/booking/2030";
+
+        // 2 - Eger soruda bize verilmisse Expected Data hazirla.
+
+        // 3 - Bize donen Response'i Actual Data olarak kaydet.
+
+        //bir GET request gonderdigimizde donen Response’un,
+        Response response = given().when().get(url);
+        response.prettyPrint();// Responseu yazdirir
+        //status code’unun 200,
+        System.out.println("Status Code: " + response.getStatusCode()); // Response status kodunu yazdirir
+        //content type’inin application/json; charset=utf-8,
+        System.out.println("Content Type: " + response.getContentType());
+        //Server isimli Header’in degerinin Cowboy
+        System.out.println("Server Header: " + response.getHeader("Server"));
+        // status Line’in HTTP/1.1 200 OK
+        System.out.println("Status Line: " + response.getStatusLine());
+        // response suresinin 5 sn’den kisa oldugunu
+        System.out.println("Response suresi: " + response.getTime());
+        // 4 - Expected Data ile Actual datanin karsilastirmasi - Assertion.
+    }
+}
